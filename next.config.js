d00a84f5726ab3e5377580b485e2ca4f9905f80f
Dave Lilly <dave@lillys.org>
2021-09-15 10:05:27 -0500

    next.config.js cors

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
         // matching all API routes
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "https://project-steve.amprsa.net" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "Access-Control-Allow-Origin, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]

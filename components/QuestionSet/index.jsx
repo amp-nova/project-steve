25bfa4cd371b40e92f6c0f2e2ca981321a57aed4
Dave Lilly <dave@lillys.org>
2021-09-15 10:43:44 -0500

    next.config.js cors

--- components/QuestionSet/index.jsx
+++ components/QuestionSet/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {GridItem} from '@chakra-ui/react'
+import { GridItem } from '@chakra-ui/react'
 import fetch from 'cross-fetch'
 
 import {
@@ -10,13 +10,13 @@ import {
     ModalHeader,
     ModalFooter,
     ModalBody,
-    ModalCloseButton, 
+    ModalCloseButton,
     useDisclosure
-  } from "@chakra-ui/react"
+} from "@chakra-ui/react"
 import QuestionSetMultiple from '../QuestionSetMultiple'
 import QuestionSetSingle from '../QuestionSetSingle'
-  
-const QuestionSet = ({multiple, question, answers}) => {
+
+const QuestionSet = ({ multiple, question, answers }) => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const [value, setValue] = useState();
     const [checkedState, setCheckedState] = useState(
@@ -24,24 +24,24 @@ const QuestionSet = ({multiple, question, answers}) => {
     );
     const handleOnChange = (position) => {
         const updatedCheckedState = checkedState.map((item, index) =>
-          index === position ? !item : item
+            index === position ? !item : item
         );
         setCheckedState(updatedCheckedState);
     }
 
-    const onSave = () => { 
-        console.log("SAVED"); 
+    const onSave = () => {
+        console.log("SAVED");
         if (multiple.allowMultiple) {
             console.log("MULTIPLE", checkedState);
-            console.log(answers.filter((item,i) => checkedState[i]));
-            answers.filter((item,i) => checkedState[i]).forEach(element => {
+            console.log(answers.filter((item, i) => checkedState[i]));
+            answers.filter((item, i) => checkedState[i]).forEach(element => {
                 console.log("ACTION", element.action);
                 let url = `${element.action.endpoint}?email=davelilly@johndoe.com`;
                 element.action.parameters.forEach(parameter => {
                     url += `&${parameter.name}=${parameter.value}`;
                 });
                 console.log(url);
-                fetch(url, { method: 'GET'})
+                fetch(url, { method: 'GET' })
                     .then(response => { if (response.status != 200) { console.error("Request error"); } });
             });
         } else {
@@ -53,7 +53,7 @@ const QuestionSet = ({multiple, question, answers}) => {
                 url += `&${parameter.name}=${parameter.value}`;
             });
             console.log(url);
-            fetch(url, { method: 'GET'})
+            fetch(url, { method: 'GET' })
                 .then(response => { if (response.status != 200) { console.error("Request error"); } });
         }
         resetValues();
@@ -74,9 +74,9 @@ const QuestionSet = ({multiple, question, answers}) => {
     let checker = arr => arr.every(v => v === false);
 
     return (
-            <GridItem rowSpan={1} colSpan={4} textAlign={'center'}>
-                <Button onClick={onOpen}>Ask me some questions!</Button>
-                <Modal isOpen={isOpen} onClose={onClose}>
+        <GridItem rowSpan={1} colSpan={4} textAlign={'center'}>
+            <Button onClick={onOpen}>Ask me some questions!</Button>
+            <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
                 <ModalContent>
                     <ModalHeader>Steve is asking</ModalHeader>
@@ -84,26 +84,26 @@ const QuestionSet = ({multiple, question, answers}) => {
                     <ModalBody>
                         {question}
                         {
-                            multiple.allowMultiple && 
-                            <QuestionSetMultiple answers={answers} changeHandler={handleOnChange}/>
+                            multiple.allowMultiple &&
+                            <QuestionSetMultiple answers={answers} changeHandler={handleOnChange} />
                         }
                         {
-                            !multiple.allowMultiple && 
-                            <QuestionSetSingle answers={answers} valueSetter={setValue}/>
+                            !multiple.allowMultiple &&
+                            <QuestionSetSingle answers={answers} valueSetter={setValue} />
                         }
                     </ModalBody>
                     <ModalFooter>
-                    <Button colorScheme="blue" mr={3} onClick={onSave} isDisabled={(multiple.allowMultiple && checker(checkedState)) 
+                        <Button colorScheme="blue" mr={3} onClick={onSave} isDisabled={(multiple.allowMultiple && checker(checkedState))
                             || (!multiple.allowMultiple && !value)}>
-                        Save
-                    </Button>
-                    <Button mr={3} onClick={closeModal}>
-                        Skip
-                    </Button>
+                            Save
+                        </Button>
+                        <Button mr={3} onClick={closeModal}>
+                            Skip
+                        </Button>
                     </ModalFooter>
                 </ModalContent>
-                </Modal>
-            </GridItem>
+            </Modal>
+        </GridItem>
 
     )
 }

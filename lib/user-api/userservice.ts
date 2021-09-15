2ffd89ffa2768369898e8af7d6af337770d56f05
Dave Lilly <dave@lillys.org>
2021-09-15 13:28:54 -0500

    cleanup

--- lib/user-api/userservice.ts
+++ lib/user-api/userservice.ts
@@ -59,32 +59,6 @@ class UserService {
     handle(handler?: any) {
         let h = handler || ((user: UserProfile, query: any) => user)
         return async (req: any, res: any, next: any) => {
-            // await NextCors(req, res, {
-            //     // Options
-            //     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-            //     origin: '*',
-            //     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-            //  });
-
-            // res.setHeader('Access-Control-Allow-Credentials', true)
-            // res.setHeader('Access-Control-Allow-Origin', '*')
-            // // another common pattern
-            // // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-            // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-            // res.setHeader(
-            //   'Access-Control-Allow-Headers',
-            //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-            // )
-            // if (req.method === 'OPTIONS') {
-            //   res.status(200).end()
-            //   return
-            // }
-
-            if (req.method === 'OPTIONS') {
-                res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-                return res.status(200).send('ok');
-            }
-
             let query = _.merge(req.body, req.query)
             let user = this.getUser(query.email)
 
--- lib/utils/config.ts
+++ lib/utils/config.ts
@@ -1,6 +1,7 @@
 const nconf = require('nconf')
-const interestInterval: number = process.env.interest_interval ? parseInt(process.env.interest_interval) : 60
-nconf.argv().env().defaults({
-    interestInterval: interestInterval * 100
-})
+const interestInterval: number = process.env.interest_interval ? parseInt(process.env.interest_interval) : 6
+
+// the interest interval is expressed in seconds, we need milliseconds (*1000)
+nconf.argv().env().defaults({ interestInterval: interestInterval * 1000 })
+
 export default nconf
